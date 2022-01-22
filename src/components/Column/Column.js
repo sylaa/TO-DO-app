396020f63b298abb18fee19b641bc5808de89392
Sylwia Ziemińska <sylwia.zieminskaa@gmail.com>
2022-01-22 11:05:35 +0100

    reading data from the store

--- src/components/Column/Column.js
+++ src/components/Column/Column.js
@@ -1,8 +1,13 @@
 import Card from '../Card/Card.js';
 import styles from './Column.module.scss';
 import CardForm from '../CardForm/CardForm.js';
+import { useSelector } from 'react-redux';
+
 
 const Column = props => {
+  const cards = useSelector(state => state.cards.filter(card => card.columnId === props.id));
+
+
   return (
     <section className={styles.column}>
       <h2 className={styles.title}>
@@ -10,7 +15,7 @@ const Column = props => {
         {props.title}
       </h2>
       <ul className={styles.cards}>
-        {props.cards.map(card => <Card key={card.id} title={card.title} />)}
+        {cards.map(card => <Card key={card.id} title={card.title} />)}
       </ul>
       <CardForm columnId={props.id} action={props.action} />
 
