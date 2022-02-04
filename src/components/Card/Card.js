33ee3e3806aeb36db15a9fa3588829c842a41ad2
Sylwia Ziemińska <sylwia.zieminskaa@gmail.com>
2022-02-04 19:51:30 +0100

    Added REMOVE_CARDS action

--- src/components/Card/Card.js
+++ src/components/Card/Card.js
@@ -1,21 +1,28 @@
 import styles from './Card.module.scss';
 import clsx from 'clsx';
 import { useDispatch } from 'react-redux';
-import { toggleFavorite } from '../../redux/cardsRedux';
+import { toggleFavorite, removeCard } from '../../redux/cardsRedux';
 
 const Card = props => {
 
     const dispatch = useDispatch();
 
-    const handleClick = () => {
+    const checkFavorite = () => {
         dispatch(toggleFavorite(props.id));
     }
 
+    const remove = () => {
+        dispatch(removeCard(props.id))
+    }
+
+
+
     return (
         <li className={styles.card}>{props.title}
-            <button onClick={handleClick} className={clsx('fa fa-star-o' , styles.button, props.isFavorite && styles.isFavorite)} >
-
-            </button>
+            <div>
+                <button onClick={checkFavorite} className={clsx('fa fa-star-o' , styles.button, props.isFavorite && styles.isFavorite)}></button>
+                <button onClick={remove} className={clsx('fa fa-trash', styles.button)} />
+            </div>
         </li>
     );
 };
--- src/redux/cardsRedux.js
+++ src/redux/cardsRedux.js
@@ -1,5 +1,5 @@
-import shortid from "shortid";
-import strContains from "../utils/strContains";
+import shortid from 'shortid';
+import strContains from '../utils/strContains';
 
 //selectors
 export const getFilteredCards = ({ cards, searchString }, columnId) =>
@@ -8,13 +8,14 @@ export const getFilteredCards = ({ cards, searchString }, columnId) =>
       card.columnId === columnId && strContains(card.title, searchString)
   );
 
-  export const getFavorites = ({ cards }, isFavorite) =>
+export const getFavorites = ({ cards }, isFavorite) =>
   cards.filter((card) => card.isFavorite === true);
 
 //actions
-const createActionName = actionName => `app/cards/${actionName}`;
+const createActionName = (actionName) => `app/cards/${actionName}`;
 const ADD_CARD = createActionName('ADD_CARD');
 const TOGGLE_CARD_FAVORITE = createActionName('TOGGLE_CARD_FAVORITE');
+const REMOVE_CARD = createActionName('REMOVE_CARD');
 
 //action creators
 export const addCard = (payload) => ({ type: ADD_CARD, payload });
@@ -24,20 +25,23 @@ export const toggleFavorite = (payload) => ({
   payload,
 });
 
+export const removeCard = (payload) => ({ type: REMOVE_CARD, payload });
 
 const cardsReducer = (statePart = [], action) => {
-    switch (action.type) {
-      case ADD_CARD:
-        return [...statePart, { ...action.payload, id: shortid() }];
-      case TOGGLE_CARD_FAVORITE:
-        return statePart.map((card) =>
-          card.id === action.payload
-            ? { ...card, isFavorite: !card.isFavorite }
-            : card
-        );
-      default:
-        return statePart;
-    }
-  };
+  switch (action.type) {
+    case ADD_CARD:
+      return [...statePart, { ...action.payload, id: shortid() }];
+    case REMOVE_CARD:
+      return statePart.filter((card) => card.id !== action.payload);
+    case TOGGLE_CARD_FAVORITE:
+      return statePart.map((card) =>
+        card.id === action.payload
+          ? { ...card, isFavorite: !card.isFavorite }
+          : card
+      );
+    default:
+      return statePart;
+  }
+};
 
-  export default cardsReducer
+export default cardsReducer;
